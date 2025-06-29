c486be805172827523604af58996352a8a4e62d2
waqas rafiq <wm66179@gmail.com>
2025-06-29 23:55:20 +0500

    create service section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,20 @@ export default {
       move: {
         "50%": { transform: "translateY(-1rem)" },
       },
+      rotate: {
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(360deg)" },
+      },
+      scaleup: {
+        "0%": { transform: "scale(0.8)" },
+        "50%": { transform: "scale(1.2)" },
+        "100%": { transform: "scale(0.8)" },
+      },
     },
     animation: {
       movingY:"move 3s linear infinite",
+      rotating:"rotate 15s linear infinite",
+      scalingUp:"scaleup 3s linear infinite",
     },
     fontFamily : {
       Jost : ['Josefin Sans', 'sans-serif'],
