4fe0d7961cea2e8dd2a59311a8ef2030d9761911
waqas rafiq <wm66179@gmail.com>
2025-06-30 23:52:42 +0500

    almost complete

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ navLinks.forEach((link) => {
     humburger.classList.remove("ri-close-large-line");
   });
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
